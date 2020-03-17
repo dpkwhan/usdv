ea27ecd545693b85769e5dd965a1eec961e84c3f
David Z. Han <zhaohui.han@gmail.com>
2020-03-16 22:06:15 -0400

    Updates

---
+++ precache-manifest.9ed9fda55288e31e023c8363e4a093b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27d500032d695b5b7797580288fc4b2f",
+    "url": "/usdv/index.html"
+  },
+  {
+    "revision": "9df8b83f8ea6d342eaf3",
+    "url": "/usdv/static/css/2.a2a67bf5.chunk.css"
+  },
+  {
+    "revision": "50df96048c78bd0dd107",
+    "url": "/usdv/static/css/main.bb719e6d.chunk.css"
+  },
+  {
+    "revision": "9df8b83f8ea6d342eaf3",
+    "url": "/usdv/static/js/2.ae160caa.chunk.js"
+  },
+  {
+    "revision": "d75b53a7922cc5e6f636829461375efb",
+    "url": "/usdv/static/js/2.ae160caa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50df96048c78bd0dd107",
+    "url": "/usdv/static/js/main.f549ec43.chunk.js"
+  },
+  {
+    "revision": "36dc1b0a2cff45c4515d",
+    "url": "/usdv/static/js/runtime-main.c2ffbd97.js"
+  }
+]);
